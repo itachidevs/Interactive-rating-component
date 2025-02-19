87aa35069f5fb8926d28d6fa256e8ecfc0d5c617
ItachiCodes <152834101+itachidevs@users.noreply.github.com>
2025-02-19 13:51:47 +0000

    added more responsive

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 const rateBin=document.getElementById('rateBtn');
 const image=document.getElementById('card-image');
 console.log(rateBin)
-const RATINGS=[1,2,3,4,5]
+const RATINGS=[
+    {
+        rate:1,
+        emoji:"😀"
+    },
+    {
+        rate:2,
+        emoji:"😊"
+    }, {
+        rate:3,
+        emoji:"😉"
+    }, {
+        rate:4,
+        emoji:"😎"
+    }, {
+        rate:5,
+        emoji:"🥳"
+    },
+
+]
 const form=document.getElementById('form');
 var rating=0;
 const submitcard=document.getElementById('submitcard');
@@ -15,27 +34,31 @@ function handleEvent(event)
 }
 const showthankyou=(event)=>{
     ratingEl.textContent=rating;
+    document.getAnimations()
     submitcard.classList.add('hidesubitcard');
+
     thankyoucard.classList.add('shothankyou');
 
 }
 const onrateSelect=(event)=>{
     event.target.classList.add('rate-btn-clicked');
-    rating=event.target.value;
+    console.log(RATINGS[event.target.value-1].emoji)
+    rating=event.target.value + "  " + RATINGS[event.target.value-1].emoji;
 rateButtons.forEach((eaach)=>{
   
         if(eaach.classList.contains('rate-btn-clicked') && eaach!==event.target)
            
             eaach.classList.toggle('rate-btn-clicked');
-    })
+    });
+
 }
 
 for(let i=0;i<5;i++)
 {
     const btn=document.createElement("button");
     btn.classList.add('rate-btn');
-    btn.value=RATINGS[i];
-    btn.textContent=RATINGS[i];
+    btn.value=RATINGS[i].rate;
+    btn.textContent=RATINGS[i].rate;
     rateBin.appendChild(btn);
     btn.addEventListener('click',onrateSelect)
 }
